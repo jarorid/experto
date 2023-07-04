977fdca8e4db72df9fc2fef447bf81e352e27840
jahir <jarorid@gmail.com>
2023-07-04 17:22:59 -0500

    endpoint que reciba un json y devuelve un numero

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 app.debug = True
@@ -8,7 +9,7 @@ app.debug = True
 def hello():
     return 'Hello World'
 
-#crea un endpoint que una cadena de texto y la devuelva en mayusculas
+
 @app.route('/mayusculas/<string:cadena>', methods=['GET'])
 def mayusculas(cadena): 
     print (cadena)
@@ -17,12 +18,43 @@ def mayusculas(cadena):
     return jsonify({"resultado": cadena})
 
 
-#crea un endpoint que reciba dos numeros y los sume
+
 @app.route('/sumar/<int:numero1>/<int:numero2>', methods=['GET'])
 def sumar(numero1, numero2):
     suma = numero1 + numero2
     print (suma)
     return jsonify({"resultado": suma})
+
+
+@app.route('/json', methods=['POST'])
+def json():
+    """
+    Maneja una solicitud POST en la ruta '/json' para procesar datos JSON.
+
+    Se espera que la solicitud contenga datos JSON con la clave 'numero1'.
+    El valor asociado con 'numero1' se utiliza para calcular el resultado.
+
+    Returns:
+        flask.Response: La respuesta JSON que contiene el resultado calculado.
+
+    Example:
+        >>> # Enviar una solicitud POST con datos JSON: {"numero1": 10}
+        >>> # La respuesta esperada sería: {"resultado": 10}
+        >>> response = client.post('/json', json={"numero1": 10})
+        >>> print(response.json)
+        {"resultado": 10}
+        >>>
+        >>> # Ejemplo de solicitud cURL equivalente
+        >>> # Enviar una solicitud POST con datos JSON: {"numero1": 20}
+        >>> # La respuesta esperada sería: {"resultado": 20}
+        >>> curl_command = 'curl -X POST -H "Content-Type: application/json" -d \'{"numero1": 20}\' http://localhost:5000/json'
+        >>> response = subprocess.check_output(curl_command, shell=True, text=True)
+        >>> print(response)
+        {"resultado": 20}
+    """
+    data = request.get_json()
+    resultado = data["numero1"]
+    return jsonify({"resultado": resultado})
 
 if __name__ == '__main__':
     app.run()
